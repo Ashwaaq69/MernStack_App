83594de570cc3d694523d0aa33df82ea019ec2cf
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-03-16 17:21:04 +0300

    created order functionalties

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -19,31 +19,47 @@ saveUserAddress,
  getUserCart,
  emptyCart,
  applyCoupon,
+ createOrder, 
+getAllOrder, 
+getSingleOrder, 
+getRecentOrders, 
+updateOrder, 
+getOrderStatusEnum,
+deleteOrder
 } = require("../controller/userCtrl"); 
 
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware"); 
 const router = express.Router();
 
 router.post("/register", createUser);
+router.post("/create-order", authMiddleware, createOrder);
 router.post("/forgot-password-token", forgotPasswordToken);
-
-router.put("/reset-password/:token", resetPassword);
-router.put("/password", authMiddleware, updatePassword);
 router.post("/login", loginUserCtrl); 
 router.post("/login-admin", loginAdmin);
 router.post("/cart", authMiddleware, userCart);
 router.post("/cart/applyCoupon",authMiddleware,applyCoupon);
+
 router.get("/all-users", getallUser)
 router.get("/getcart", authMiddleware, getUserCart)
 router.get("/refresh", handleRefreshToken)
 router.get("/logout", logout);
 router.get("/wishlist", authMiddleware, getWishlist )
 router.get("/:id", authMiddleware, isAdmin, getaUser)
+router.get("/order-status-enum", getOrderStatusEnum)
+router.get("/all-orders", authMiddleware, isAdmin, getAllOrder)
+router.get("/recent-orders", authMiddleware, getRecentOrders)
+router.get("/order/:id", authMiddleware, getSingleOrder)
+
 router.delete("/:id", deleteaUser)
 // router.delete("/empty-cart/:id", authMiddleware, emptyCart);
 router.delete("/empty-cart", authMiddleware , emptyCart);
+router.delete("/order/:id", authMiddleware, deleteOrder)
+
+router.put("/reset-password/:token", resetPassword);
+router.put("/password", authMiddleware, updatePassword);
 router.put("/edit-user", authMiddleware, updateaUser)
 router.put("/save-adress", authMiddleware, saveUserAddress )
+router.put("/order/:id", authMiddleware, updateOrder)
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser)
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser)
 
